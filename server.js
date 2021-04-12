6b50833ac6617f33438b874763d815f2bf681c79
miaherman <mia.herman@live.se>
2021-04-12 17:35:34 +0200

    put, get, post etc

--- server.js
+++ server.js
@@ -1,74 +1,119 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-
 const myfavoritesweets = [
-   {
-       id: 1,
-       name: "Pavlova", 
-       origin: "New Zealand",
-       inventor: "Helen Leach"
+    {
+        "id": 1,
+        "name": "kola", 
+        "origin": "New Zealand",
+        "inventor": "Helen Leach"
+    },
+    {
+        "id": 2,
+        "name": "Creme brulee", 
+        "origin": "New Zealand",
+        "inventor": "Helen Leach"
+    }
 
-   },
-   {
-       id: 2,
-       name: "Crème Brûlée",
-       origin: "France",
-       inventor: "François Massialot"
-   },
-   {
-       id: 3,
-       name: "Gelato",
-       origin: "Italy",
-       inventor: "Bernando Buontalenti "
-   }
-]
+];
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
-});
+app.use(express.json());
 
 app.get('/api/favoritesweets', (req, res) => {
     res.send(myfavoritesweets);
 });
 
+// hittar sweet by ID
+
 app.get('/api/favoritesweets/:id', (req, res) => {
     const sweet = myfavoritesweets.find(c => c.id === parseInt(req.params.id));
-    if(!sweet) res.status(404).send('The sweet does not exist!');
+    if(!sweet) res.status(404).send('This sweet does not exist!');
     res.send(sweet);
 });
 
-app.post('/api/favoritesweets', (req, res) => {
-    if (!req.body.name || req.body.name.length < 3) {
-        res.status(400).send('Name is req and should be min 3 characters');
-        return;
+app.get("/api", (req, res) => {
+    res.json(myfavoritesweets)
+})
+
+//Lägger till ny sweet
+app.post("/api/favoritesweets/", (req, res) => {
+
+    if (!req.body.name) {
+        res.json({"Error": "does not exist"})
+        return
     }
 
-    const sweet = {
-        id: myfavoritesweets.lenght + 1,
-        name: req.body.name
-    };
-    myfavoritesweets.push(sweet);
-    res.send(sweet);
+    let titleToSave = req.body.name
+    let originToSave = req.body.origin
+    let inventorToSave = req.body.inventor
+
+    let idToSave = 0
+    myfavoritesweets.forEach((sweet)=> {
+        if (sweet.id > idToSave) {
+            idToSave = sweet.id
+        }
+    })
+
+    idToSave++
+
+    myfavoritesweets.push({
+        id: idToSave,
+        name: titleToSave,
+        origin: originToSave,
+        inventor: inventorToSave
+    })
+
+    res.json({
+        status: "You added a new sweet!"
+    })
+})
+
+app.delete('/api/favoritesweets/:id', (req, res) => {
+    const index = myfavoritesweets.find(p => p.id == 1);
+    let deletedProduct = myfavoritesweets.splice(index, 1);
+    
+    if(!index) {
+        res.status(404).json({"Error": "This sweet does not exist!"});
+        return;
+    }
+    res.json(deletedProduct);
 });
 
-//Lägga till ny sweet
-// app.put('api/courses/:id', (req, res) => {
-//     const sweet = myfavoritesweets.find(c => c.id === parseInt(req.params.id));
-//     if(!sweet) res.status(404).send('The sweet does not exist!');
+//Uppdaterar sötsak
 
-//     if (!req.body.name || req.body.name.length < 3) {
-//         res.status(400).send('Name is req and should be min 3 characters');
-//         return;
-//     }
+app.put('/api/favoritesweets/:id', (req, res) => {
+    let updateSweet = myfavoritesweets.findIndex((sweet) => {
+        return sweet.id == req.params.id
+    })
 
-//     sweet.name = req.body.name;
-//     res.send(sweet);
-// });
+    updateSweet = {
+        id: req.body.id,
+        name: req.body.name,
+        origin: req.body.origin,
+        inventor: req.body.inventor
+    };
+
+    let index = myfavoritesweets.findIndex((sweet) => {
+        return sweet.id == req.params.id
+    });
+
+    myfavoritesweets[index] = updateSweet;
+    return res.send(myfavoritesweets);
+});
+
 
 const port = process.env.PORT || 3000;
 
+//lyssnar på servern
 app.listen(port, () => {
     console.log(`listening to port ${port}`)
-})
+});
+
+
+
+
+// app.post('/api/favoritesweets/', (req, res) => {
+//     const sweet = {...req.body, id: Math.round(Math.random() * 1000)}
+//     myfavoritesweets.push(sweet);
+//     res.status(201).json(sweet);
+// })
